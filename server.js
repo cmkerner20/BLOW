34b447731328df917624445086f4ceec83c54300
Scott Armstrong <armstrong@Scott-Armstrongs-MacBook-2.local>
2016-02-18 14:35:17 -0500

    got npm stuff

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database('garage_demo.db');
+var app      = express();
+
+app.use(express.static("./public")); // sets standard files things. i.e /public/imgs will be /imgs
+
 
 db.run("PRAGMA foreign_keys = ON;"); //enables foreign keys in sqlite3
 
@@ -39,4 +43,11 @@ db.all('SELECT * FROM cars', function(err, rows){
   	console.log('********CARS TABLE**********');
     console.log(rows); 
   }
+});
+
+require('./routes/routes.js')(app);//(app); // load our routes and pass in our app and fully configured passport
+
+// launch ======================================================================
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
