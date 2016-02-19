e6afd4e2bd13fd3d67b33cd7c8a0fa63f564e1c4
Scott Armstrong <armstrong@Scott-Armstrongs-MacBook-2.local>
2016-02-19 12:32:50 -0500

    part 4

--- routes/routes.js
+++ routes/routes.js
@@ -4,5 +4,15 @@ app.get('/', function(req, res){
   res.render('index.ejs');
 });
 
+app.get('/login', function(req, res) {
+    // render the page and pass in any flash data if it exists
+    res.render('login.ejs');
+});
+
+app.get('/register', function(req, res) {
+     // render the page and pass in any flash data if it exists
+    res.render('register.ejs');
+});
+
 }
 
