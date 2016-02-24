d4dcda41c3b33a00ca9cbb7c1dd5531731957c8e
Catherine <cmkerner20@gmail.com>
2016-02-24 10:20:41 -0500

    adhoc

--- seeds.js
+++ seeds.js
@@ -10,7 +10,7 @@ db.run("INSERT INTO users (id, username, password) VALUES (?, ?, ?)",
   }
 );
 
-<<<<<<< HEAD
+
 db.run("INSERT INTO bills (id, name) VALUES (?, ?)",
   1, 'thebill',
   function(err) {
@@ -34,13 +34,13 @@ db.run("INSERT INTO user_bills (id_user, id_bill) VALUES (?, ?)",
 
 db.run("INSERT INTO user_legislators (id_user, id_legislator) VALUES (?, ?)",
   1, 1,
-=======
+
 db.run("INSERT INTO cars (garage_id, make, model, year) VALUES (?,?,?,?), (?,?,?,?), (?,?,?,?),(?,?,?,?)",
   1, 'Lamborghini', 'Countach', 1986,
   1, 'Citroen', 'SM', 1970,
   1, 'Mazda', 'Cosmo', 1970,
   1, 'Jaguar', 'E-Type', 1963,
->>>>>>> e6afd4e2bd13fd3d67b33cd7c8a0fa63f564e1c4
+//>>>>>>> e6afd4e2bd13fd3d67b33cd7c8a0fa63f564e1c4
   function(err) {
     if (err) { throw err;}
   }
